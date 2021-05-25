e7450ff6a9ce7a1c19640387c2cd8e692a196f80
David <DavidLegrand@users.noreply.github.com>
2021-05-25 16:46:45 +0200

    First Commit

---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css'],
+})
+export class TaskComponent implements OnInit {
+  name: string = 'Faire la vaisselle';
+  complete: boolean = false;
+  className = 'list-group-item';
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  getComplete(): string {
+    return this.complete ? 'Terminée' : 'En cours';
+  }
+
+  getItemVariant(): string {
+    return this.complete
+      ? 'list-group-item-success'
+      : 'list-group-item-warning';
+  }
+
+  getBadgeVariant(): string {
+    return this.complete ? 'bg-success' : 'bg-warning text-dark';
+  }
+
+  getBtnTxt(): string {
+    return this.complete ? 'Annuler' : 'Terminer';
+  }
+  
+  toggleComplete(): void {
+    this.complete = !this.complete;
+  }
+}
